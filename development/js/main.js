512e2ba8f12f2b2a5d04484dcbd1355790cc8b2a
shainanigans <me@shainakoval.com>
2015-12-11 15:26:57 +1100

    feat: Add count of clicks

--- development/js/main.js
+++ development/js/main.js
@@ -1,1 +1,12 @@
+$(function() {
 
+    // Count clicks
+    var clicks = 0;
+    $('.catimage').click(function() {
+        // Increment by one each click
+        clicks++;
+        // Remove old count and append new count to page
+        $('.clicks__count').text('').append(clicks);
+    });
+
+})
