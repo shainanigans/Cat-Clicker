d2c60c57cd295941187bae5539943838bc8a3f14
shainanigans <me@shainakoval.com>
2015-12-14 09:43:22 +1100

    feat: Add second cat

--- development/js/main.js
+++ development/js/main.js
@@ -1,5 +1,27 @@
 $(function() {
 
+    //Cat objects
+    var cats = {
+        'cats': [
+            {
+                'image' : 'img/cat-on-amp.jpg',
+                'alt' : 'An adorable tabby kitty on an amp',
+                'name' : 'Ampelina'
+            },
+            {
+                'image' : 'img/cat-on-floor.jpg',
+                'alt' : 'A cute orange kitty on the floor',
+                'name' : 'Floora'
+            }
+        ]
+    }
+    console.log(cats.cats.length);
+
+    //Append cats to page
+    for (i = 0; i < cats.cats.length; i++) {
+        $('#cat-container').append('<div class="cat flex-item--1-2"><p class="catname">' + cats.cats[i].name + '</p><img class="catimage" src="' + cats.cats[i].image + '" alt="' + cats.cats[i].alt + '"></div>');
+    }
+
     // Count clicks
     var clicks = 0;
     $('.catimage').click(function() {
